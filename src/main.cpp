cfb67fcae54f867a74b7ba77d530260e5787040d
hkaraoguz <hkaraoguz@yahoo.com.tr>
2013-06-27 20:45:37 +0300

    random sayilar poisson dagilimdan cekiliyor.

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,115 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <navigationISL/hotspot.h>
-#include<iostream>
-#include<ctime>
+#include <iostream>
+#include <ctime>
 #include <sstream>
+
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+#include <QDebug>
+#include <qjson/parser.h>
+#include <QDir>
+#include <QFile>
+
+double mu;
+
+// Reads the config file
+bool readConfigFile(QString filename)
+{
+    QFile file(filename);
+
+    if(!file.exists()) return false;
+
+    if(!file.open(QFile::ReadOnly)) return false;
+
+    QJson::Parser parser;
+
+    bool ok;
+
+    QVariantMap result = parser.parse(&file,&ok).toMap();
+
+    if(!ok){
+
+        file.close();
+        qDebug()<<"Fatal reading error";
+
+        return false;
+    }
+    else
+    {
+
+        mu  = result["poissonMU"].toDouble();
+
+        qDebug()<< " mu " << mu;//result["poissonMU"].toString();
+
+    }
+    file.close();
+    return true;
+
+}
+
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line. For programmatic
-   * remappings you can use a different version of init() which takes remappings
-   * directly, but for most command-line programs, passing argc and argv is the easiest
-   * way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
   ros::init(argc, argv, "hotspotobserverISL");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
   ros::Publisher hotspotPublisher = n.advertise<navigationISL::hotspot>("hotspotobserverISL/hotspot", 1000);
 
-  ros::Rate loop_rate(5);
+  ros::Rate loop_rate(1);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
+  QString path = QDir::homePath();
+  path.append("/fuerte_workspace/sandbox/configISL.json");
 
-  int hotspotThreshold = 0.005*RAND_MAX;
+
+  if(!readConfigFile(path)){
+
+      qDebug()<< "Read Config File Failed!!!";
+
+      ros::shutdown();
+
+      return 0;
+  }
+
+
+//  int count = 0;
+
+//  int hotspotThreshold = 0.005*RAND_MAX;
+
+  const gsl_rng_type * T;
+  gsl_rng * r;
+  gsl_rng_env_setup();
+  T = gsl_rng_default;
+  r = gsl_rng_alloc(T);
+  int prn;
 
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
+      prn = gsl_ran_poisson(r, mu);
+      if(prn>=1){
 
+          std::time_t t = std::time(0);  // t is an integer type
+
+          //std::cout << t ;
+          //std::cout << " prn "<< prn << " time " << t;
+
+          qDebug()<< " time " << t;
+
+         navigationISL::hotspot hs;
+
+         hs.hotspot = t;
+
+
+   // ROS_INFO("%s", msg.data.c_str());
+
+        hotspotPublisher.publish(hs);
+    }
+
+  /*
       if(rand()%RAND_MAX < hotspotThreshold){
 
           std::time_t t = std::time(0);  // t is an integer type
@@ -74,14 +123,9 @@ int main(int argc, char **argv)
 
    // ROS_INFO("%s", msg.data.c_str());
 
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
         hotspotPublisher.publish(hs);
     }
+  */
     ros::spinOnce();
 
     loop_rate.sleep();
